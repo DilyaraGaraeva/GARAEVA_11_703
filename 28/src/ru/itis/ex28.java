ff31f0b50e96284612d684c0658d5517f8edf177
Dilyara <dilyara_99@inbox.ru>
2017-10-23 20:10:49 +0300

    task28

---
+++ 28/src/ru/itis/ex28.java
@@ -0,0 +1,24 @@
+package ru.itis;
+import java.util.Scanner;
+public class ex28 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double x, b0, b1, a0, a1, e;
+                x = in.nextDouble();
+                b0 = 1 - x;
+                a0 = x;
+                e = 0.0001;
+
+        a1 = a0*(1 + b0/2);
+        b1 = b0*b0*(3 + b0);
+
+        while(b1 > e){
+            a0 = a1;
+            b0 = b1;
+            a1 = a0*(1 + b0/2);
+            b1 = b0*b0*(3 + b0);
+        }
+
+        System.out.println(a1);
+    }
+}
