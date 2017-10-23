307126a9271db214e6ff40d86d39ca5bc01bcd84
Dilyara <dilyara_99@inbox.ru>
2017-10-23 20:03:56 +0300

    task20d

---
+++ 29a/src/ru/itis/Main.java
@@ -0,0 +1,21 @@
+package ru.itis;
+import java.util.Scanner;
+class homework29a {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+        double x, e, y0, y1;
+        x = in.nextDouble();
+        e = 0.0001;
+        if(x >= 1) y0 = x/3; else y0 = x;
+        y1 = y0 -(y0 - x/(y0*y0))/3;
+
+        while(Math.abs(y0 - y1) > e) {
+            y0 = y1;
+            y1 = y0 -(y0 - x/(y0*y0))/3;
+            i++;
+        }
+        System.out.println(i);
+        System.out.println(y1);
+    }
+}
---
+++ untitled/src/ru/itis/Main.java
@@ -0,0 +1,8 @@
+package ru.itis;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
