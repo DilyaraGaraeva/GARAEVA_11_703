b1e5076391623c039017150318c0525bc3a038f9
Dilyara <dilyara_99@inbox.ru>
2017-12-19 12:00:08 +0300

    jkg

---
+++ Stroki/Task23.2/src/ru/itis/Main.java
@@ -0,0 +1,30 @@
+﻿package ru.itis;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String s = sc.nextLine();
+        System.out.println(uniqueCount(s.toCharArray()));
+    }
+    public static String uniqueCount(char[] x) {
+        String result="";
+        boolean countUnique = true;
+        for (int i = 0; i < x.length; i++) {
+            countUnique = true;
+            for (int j = 0; j < x.length; j++) {
+                if (x[j]==x[i] && j != i) {
+                    countUnique = false;
+                    break;
+                }
+            }
+            if (countUnique) {
+                result+=x[i];
+            }
+        }
+        return result;
+    }
+}
---
+++ Stroki/Task23.4/src/ru/itis/Main.java
@@ -0,0 +1,25 @@
+﻿package ru.itis;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите двоичное число ");
+        String text = sc.nextLine();
+        while (isBinaryString(text)!=true){
+            System.out.println("Ввод неверен введите двоичное число ");
+            text = sc.nextLine();
+        }
+        System.out.println("Десятичное число "+Integer.parseInt(text,2));
+    }
+    public static boolean isBinaryString(String text){
+        String t = "((0|1).*)";
+        Pattern pattern = Pattern.compile(t);
+        Matcher reg = pattern.matcher(text);
+        return reg.matches();
+    }
+}
